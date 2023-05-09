be5a810f3d3b5f690272655dde2efdd45f702241
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 18:07:17 -0300

    Clase Main ej1
    Prueba de objetos

--- TercerCuatri-Guia4/src/ejercicioUnoTest/Main.java
+++ TercerCuatri-Guia4/src/ejercicioUnoTest/Main.java
@@ -1,5 +1,8 @@
 package ejercicioUnoTest;
 
+import ejercicioUno.Cuadrado;
+import ejercicioUno.FiguraGeometrica;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,6 +10,19 @@ public class Main {
         una interfaz "Figura" que contenga los métodos comunes a todas las formas, cómo calcular el área y el perímetro.
         Además, queremos definir una clase anidada "Punto" que representa las coordenadas (x,y) de los vértices de las
         formas.*/
+
+        Cuadrado cuadrado = new Cuadrado("Cuadrado", 10.0d);
+
+        System.out.println(cuadrado.getAnchoAlto());
+
+        System.out.println(cuadrado.calcularArea());
+
+        System.out.println(cuadrado.calcularPerimetro());
+
+        Cuadrado.Punto puntos = cuadrado.new Punto(20,10);
+
+        System.out.println(puntos.mostrarPuntos());
+
 
     }
 }
