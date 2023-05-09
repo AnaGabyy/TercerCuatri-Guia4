2c6a753949a5eaa5c92e27bb7aa3cf8b7e5aa5ce
AnaGabyy <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 10:41:47 -0300

    Clase Main ej2
    Enunciado

---
+++ TercerCuatri-Guia4/src/ejercicioDosTest/Main.java
@@ -0,0 +1,13 @@
+package ejercicioDosTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Supongamos que estamos trabajando en un programa que modela un ecosistema en el que existen diferentes
+        animales. Queremos definir una interfaz "Elemento" que contenga los métodos comunes a todos los elementos
+        del ecosistema, como por ejemplo la posición en el espacio y la representación gráfica. Además, queremos
+        que los Elefantes están especificados, queremos definir una clase anidada "Coordenada" que representa las
+        coordenadas (x,y,z) de los elementos.*/
+
+    }
+}
