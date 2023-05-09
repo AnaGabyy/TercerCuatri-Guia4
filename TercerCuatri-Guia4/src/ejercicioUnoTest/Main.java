a2c3704a74733f32c92243666f13ed50c687c1fb
AnaGabyy <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 10:41:59 -0300

    Clase Main ej1
    Enunciado

---
+++ TercerCuatri-Guia4/src/ejercicioUnoTest/Main.java
@@ -0,0 +1,12 @@
+package ejercicioUnoTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Supongamos que estamos trabajando en un programa que maneja diferentes formas geométricas, y queremos definir
+        una interfaz "Figura" que contenga los métodos comunes a todas las formas, cómo calcular el área y el perímetro.
+        Además, queremos definir una clase anidada "Punto" que representa las coordenadas (x,y) de los vértices de las
+        formas.*/
+
+    }
+}
