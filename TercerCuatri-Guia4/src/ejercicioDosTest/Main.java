4abd30eb6ad6f8870078b0738ca1f5c3afabc5cb
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 18:07:52 -0300

    Clase Main ej2
    Prueba de objeto y metodo mostrarCoordenadas

--- TercerCuatri-Guia4/src/ejercicioDosTest/Main.java
+++ TercerCuatri-Guia4/src/ejercicioDosTest/Main.java
@@ -1,5 +1,7 @@
 package ejercicioDosTest;
 
+import ejercicioDos.Animal;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,7 +9,14 @@ public class Main {
         animales. Queremos definir una interfaz "Elemento" que contenga los métodos comunes a todos los elementos
         del ecosistema, como por ejemplo la posición en el espacio y la representación gráfica. Además, queremos
         que los Elefantes están especificados, queremos definir una clase anidada "Coordenada" que representa las
-        coordenadas (x,y,z) de los elementos.*/
+        coordenadas (x, y, z) de los elementos.*/
+
+
+        Animal elefante = new Animal("Elefante Loxodonta Africana");
+        Animal.Coordenada coordenada = elefante.new Coordenada(10.0d, 55.6d, 45.1d);
+
+        coordenada.mostrarCoornedadas();
+
 
     }
 }
